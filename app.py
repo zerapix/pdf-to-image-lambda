f401d61e13709226f44708b9db41f75ccf85b71a
timothyhunt <timhunt12@gmail.com>
2021-03-29 17:27:17 -0600

    1. modified config.json to match our S3 bucket. 2. modified the app.py to put create the jpeg from the pdf with the same name if the number of pages is 1 and number the pages if greater than 1. 3. updated the requirements.txt to include poppler. 4. Added to AWS Lambda Layers a poppler layer to work with this code.

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ else:
     logging.info(f"No DPI environment variable set.  Using the default: DPI=300")
 
 _SUPPORTED_IMAGE_EXTENSIONS = ["ppm", "jpeg", "png", "tiff"]
-FMT = "png"
+FMT = "jpeg"
 if 'FMT' in os.environ:
     environ_fmt = str(os.environ['FMT'])
     if environ_fmt in _SUPPORTED_IMAGE_EXTENSIONS:
@@ -29,7 +29,7 @@ if 'FMT' in os.environ:
                       f"Only the following formats are supported: {','.join(_SUPPORTED_IMAGE_EXTENSIONS)}.  "
                       f"Using the default: FMI='png'")
 else:
-    logging.info(f"No FMT environment variable set.  Using the default: FMT='png'")
+    logging.info(f"No FMT environment variable set.  Using the default: FMT='jpeg'")
 
 DESTINATION_BUCKET = None
 if 'DESTINATION_BUCKET' in os.environ:
@@ -55,7 +55,7 @@ _SUPPORTED_FILE_EXTENSION = '.pdf'
 @app.on_s3_event(bucket=ORIGIN_BUCKET,
                  events=['s3:ObjectCreated:*'])
 def pdf_to_image(event):
-    """Take a pdf fom an S3 bucket and convert it to a list of pillow images (one for each page of the pdf).
+    """Take a pdf from an S3 bucket and convert it to a list of pillow images (one for each page of the pdf).
     :param event: A Lambda event (referring to an S3 event object created event).
     :return:
     """
@@ -71,20 +71,23 @@ def pdf_to_image(event):
     logging.info("Successfully retrieved S3 object.")
 
     # Set poppler path
-    poppler_path = "/var/task/lib/poppler-utils-0.26/usr/bin"
+    # poppler_path = "/var/task/lib/poppler-utils-0.26/usr/bin"
     images = convert_from_bytes(infile,
                                 dpi=DPI,
-                                fmt=FMT,
-                                poppler_path=poppler_path)
+                                fmt=FMT)
     logging.info("Successfully converted pdf to image.")
 
+    pages = len(images)
     for page_num, image in enumerate(images):
 
         # The directory is: <name of the pdf>-num_pages-<number of pages in the pdf>
-        directory = event.key.split('.')[0] + "-num_pages-" + str(len(images))
+        # directory = event.key.split('.')[0] + "-num_pages-" + str(len(images))
 
         # Then save the image and name it: <name of the pdf>-page<page number>.FMT
-        location = directory + "/" + event.key.split('.')[0] + "-page" + str(page_num) + '.' + FMT
+        if pages < 2:
+            location = event.key.split('.')[0] + '.' + FMT
+        else:
+            location = event.key.split('.')[0] + "-" + str(page_num) + '.' + FMT
 
         logging.info(f"Saving page number {str(page_num)} to S3 at location: {DESTINATION_BUCKET}, {location}.")
 
